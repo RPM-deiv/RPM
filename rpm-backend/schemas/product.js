6314a979f73fce41c19afe049b08ca8b3f2d333e
Round0000 <billybinche@gmail.com>
2022-10-13 17:12:05 +0200

    getproducts function

---
+++ rpm-backend/schemas/product.js
@@ -0,0 +1,75 @@
+export default {
+  name: "product",
+  title: "Article",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Intitulé",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      title: "Disponibilité",
+      name: "state",
+      type: "string",
+      options: {
+        layout: "radio",
+        list: [
+          { title: "Disponible", value: "available" },
+          { title: "Réservé", value: "engaged" },
+          { title: "Vendu", value: "sold" },
+        ],
+      },
+    },
+    {
+      name: "body",
+      title: "Présentation",
+      type: "text",
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "dimensions",
+      title: "Dimensions",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "price",
+      title: "Prix",
+      type: "number",
+    },
+    {
+      name: "images",
+      title: "Photos",
+      type: "array",
+      of: [
+        {
+          type: "image",
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      subtitle: "state",
+    },
+  },
+};
