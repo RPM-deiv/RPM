26f300838e0430708fbbebb6b3e1390ce425473a
Round0000 <billybinche@gmail.com>
2022-10-13 16:36:32 +0200

    sanity

--- functions/getProducts.js
+++ functions/getProducts.js
@@ -1,48 +1,48 @@
-const sanityClient = require('@sanity/client');
-const imageUrlBuilder = require('@sanity/image-url');
+// const sanityClient = require('@sanity/client');
+// const imageUrlBuilder = require('@sanity/image-url');
 
-const sanity = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  useCdn: true,
-});
+// const sanity = sanityClient({
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+//   useCdn: true,
+// });
 
-exports.handler = async () => {
-  const query = '*[_type=="product"] | order(title asc)';
-  const products = await sanity.fetch(query).then(results => {
-    const allProducts = results.map(product => {
-      const output = {
-        id: product.slug.current,
-        title: product.title,
-        url: `${process.env.URL}/.netlify/functions/getProducts`,
-        state: product.state,
-        tags: product.tags,
-        dimensions: product.dimensions,
-        price: product.price,
-        body: product.body,
-        images: [],
-      };
+// exports.handler = async () => {
+//   const query = '*[_type=="product"] | order(title asc)';
+//   const products = await sanity.fetch(query).then(results => {
+//     const allProducts = results.map(product => {
+//       const output = {
+//         id: product.slug.current,
+//         title: product.title,
+//         url: `${process.env.URL}/.netlify/functions/getProducts`,
+//         state: product.state,
+//         tags: product.tags,
+//         dimensions: product.dimensions,
+//         price: product.price,
+//         body: product.body,
+//         images: [],
+//       };
 
-      if (product.images && product.images.length > 0) {
-        product.images.forEach(img => {
-          const imgRef = img.asset._ref;
-          output.images.push({
-            small: imageUrlBuilder(sanity).image(imgRef).width(200).url(),
-            square: imageUrlBuilder(sanity).image(imgRef).size(400, 400).url(),
-            large: imageUrlBuilder(sanity).image(imgRef).width(1000).url(),
-          });
-        });
-      }
+//       if (product.images && product.images.length > 0) {
+//         product.images.forEach(img => {
+//           const imgRef = img.asset._ref;
+//           output.images.push({
+//             small: imageUrlBuilder(sanity).image(imgRef).width(200).url(),
+//             square: imageUrlBuilder(sanity).image(imgRef).size(400, 400).url(),
+//             large: imageUrlBuilder(sanity).image(imgRef).width(1000).url(),
+//           });
+//         });
+//       }
 
-      return output;
-    });
-    console.log(allProducts);
-    return allProducts;
-  });
+//       return output;
+//     });
+//     console.log(allProducts);
+//     return allProducts;
+//   });
 
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(products),
-  };
-};
+//   return {
+//     statusCode: 200,
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(products),
+//   };
+// };
