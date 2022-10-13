6c79ef0b084bf76d773c5f64f428daab0e06298f
Round0000 <billybinche@gmail.com>
2022-10-13 17:17:36 +0200

    added service and model

---
+++ src/app/models/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  title: string;
+  url: string;
+  tags: string[];
+  state: string;
+  dimensions: string;
+  price: number;
+  body: any;
+  images: any[];
+}
